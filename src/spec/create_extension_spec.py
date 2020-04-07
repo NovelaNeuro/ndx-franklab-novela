1a498806b999a4818601d9a7e9775639b119429b
acwikla <acwikla@novelaneuro.com>
2020-04-07 13:05:14 +0200

    Update version to 0.0.4

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -11,7 +11,7 @@ def main():
     ns_builder = NWBNamespaceBuilder(
         doc="""NovelaNeurotechnologies Namespaces""",
         name="""ndx-fllab-novela""",
-        version="""0.0.3""",
+        version="""0.0.4""",
         author=list(map(str.strip, """NovelaDevops""".split(','))),
         contact=list(map(str.strip, """devops@novelaneuro.com""".split(',')))
     )

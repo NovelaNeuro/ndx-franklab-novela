6619b8501ad6e8c9f1b02a18c90e52e13be2b889
acwikla <acwikla@novelaneuro.com>
2020-04-10 11:09:53 +0200

    rename_bad_channels

--- src/pynwb/ndx_fllab_novela/ntrode.py
+++ src/pynwb/ndx_fllab_novela/ntrode.py
@@ -7,12 +7,12 @@ from pynwb.ecephys import ElectrodeGroup
 @register_class('NTrode', 'ndx-fllab-novela')
 class NTrode(ElectrodeGroup):
     ''' Representation of NTrode object in NWB '''
-    __nwbfields__ = ('ntrode_id', 'probe_id', 'bad_channel','map')
+    __nwbfields__ = ('ntrode_id', 'probe_id', 'bad_channels','map')
 
     @docval(*get_docval(ElectrodeGroup.__init__) + (
             {'name': 'ntrode_id', 'type': 'int', 'doc': 'id of electrode group'},
             {'name': 'probe_id', 'type': 'int', 'doc': 'id of probe EG belongs to'},
-            {'name': 'bad_channel', 'type': 'array_data', 'doc': 'ids of bad channels'},
+            {'name': 'bad_channels', 'type': 'array_data', 'doc': 'ids of bad channels'},
             {'name': 'map', 'type': 'array_data', 'doc': 'map of ntrode'},
     ))
     def __init__(self, **kwargs):
@@ -20,11 +20,11 @@ class NTrode(ElectrodeGroup):
                             for kwargs_item in kwargs.copy()
                             if kwargs_item != 'ntrode_id'
                             if kwargs_item != 'probe_id'
-                            if kwargs_item != 'bad_channel'
+                            if kwargs_item != 'bad_channels'
                             if kwargs_item != 'map'
                             })
         call_docval_func(super(NTrode, self).__init__, kwargs)
         self.ntrode_id = kwargs['ntrode_id']
         self.probe_id = kwargs['probe_id']
-        self.bad_channel = kwargs['bad_channel']
+        self.bad_channels = kwargs['bad_channels']
         self.map = kwargs['map']

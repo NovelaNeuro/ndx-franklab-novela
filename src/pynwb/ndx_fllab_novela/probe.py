fe540c2985b220d85e66808a783080bda90994ca
acwikla <acwikla@novelaneuro.com>
2020-04-15 15:15:02 +0200

    Delete additional probes fields Update version to 0.0.6

--- src/pynwb/ndx_fllab_novela/probe.py
+++ src/pynwb/ndx_fllab_novela/probe.py
@@ -11,32 +11,26 @@ class Probe(Device):
             {'name': 'id', 'type': 'int', 'doc': 'unique id of the probe'},
             {'name': 'probe_type', 'type': 'str', 'doc': 'type of probe'},
             {'name': 'units', 'type': 'str', 'doc': 'units in device'},
-            {'name': 'probe_description', 'type': 'str', 'doc': 'probe_description of probe'},
             {'name': 'num_shanks', 'type': 'int', 'doc': 'number of shanks associated with probe'},
             {'name': 'contact_side_numbering', 'type': 'bool', 'doc': 'is contact_side_numbering enabled'},
             {'name': 'contact_size', 'type': 'float', 'doc': 'value of contact size as float'},
-            {'name': 'shanks', 'type': 'object', 'doc': 'shanks in the probe'},))
+            ))
     def __init__(self, **kwargs):
         super().__init__(**{kwargs_item: kwargs[kwargs_item]
                             for kwargs_item in kwargs.copy()
                             if kwargs_item != 'id'
                             if kwargs_item != 'probe_type'
                             if kwargs_item != 'units'
-                            if kwargs_item != 'probe_description'
                             if kwargs_item != 'num_shanks'
                             if kwargs_item != 'contact_side_numbering'
                             if kwargs_item != 'contact_size'
-                            if kwargs_item != 'shanks'
                             })
         call_docval_func(super(Probe, self).__init__, kwargs)
         self.id = kwargs['id']
         self.probe_type = kwargs['probe_type']
         self.units = kwargs['units']
-        self.probe_description = kwargs['probe_description']
         self.num_shanks = kwargs['num_shanks']
         self.contact_side_numbering = kwargs['contact_side_numbering']
         self.contact_size = kwargs['contact_size']
-        self.shanks = kwargs['shanks']
 
-    __nwbfields__ = ('id', 'probe_type', 'units', 'probe_description', 'num_shanks', 'contact_side_numbering',
-                     'contact_size', 'shanks')
+    __nwbfields__ = ('id', 'probe_type', 'units', 'num_shanks', 'contact_side_numbering', 'contact_size')

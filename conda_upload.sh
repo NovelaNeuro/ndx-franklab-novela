5569b9a1757cf0bedf1dcd7b6f5df3b553f079bf
acwikla <acwikla@novelaneuro.com>
2020-04-24 12:10:31 +0200

    upload conda

--- conda_upload.sh
+++ conda_upload.sh
@@ -25,8 +25,8 @@ conda build . -c conda-forge --no-include-recipe || exit 1
 #echo "Creating new conda package without some files..."
 #tar -cjvf ${PKG_NAME}-${VERSION}-py37_0.tar.bz2 --exclude='*.sh' --exclude='*.cfg' --exclude='*.pyc'--exclude='*.pytest_cache' --exclude='*.gitignore' info lib || exit 1
 
-echo "Move conda package to linux dir..."
-mv new_tar/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 linux-64 || exit 1
+#echo "Move conda package to linux dir..."
+#mv new_tar/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 linux-64 || exit 1
 
 echo "Converting conda package..."
 conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1

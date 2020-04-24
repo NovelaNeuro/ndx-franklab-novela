41d4cf995b9e8ce1755d0e9775aaef7902ad2364
acwikla <acwikla@novelaneuro.com>
2020-04-24 11:37:18 +0200

    upload conda

--- conda_upload.sh
+++ conda_upload.sh
@@ -10,6 +10,24 @@ conda config --set anaconda_upload no
 echo "Building conda package..."
 conda build . || exit 1
 
+echo "Move conda package..."
+mv ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-*-py37_0.tar.bz2  ${CONDA_BUILD_PATH} || exit 1
+
+echo "Making new_tar dir..."
+mkdir ${CONDA_BUILD_PATH}/new_tar || exit 1
+
+echo "Extracting conda package..."
+tar -xf ${CONDA_BUILD_PATH}/${PKG_NAME}-*-py37_0.tar.bz2 -C ${CONDA_BUILD_PATH}/new_tar || exit 1
+
+cd ${CONDA_BUILD_PATH}/new_tar || exit 1
+
+echo "Creating new conda package without some files..."
+tar -cjvf ${PKG_NAME}-*-py37_0.tar.bz2 --exclude='*.sh' --exclude='*.cfg' --exclude='*.pytest_cache' --exclude='*.gitignore' info lib || exit 1
+
+cd ..
+
+echo "Move conda package to linux dir..."
+mv new_tar/${PKG_NAME}-*-py37_0.tar.bz2 linux-64 || exit 1
 echo "Converting conda package..."
 conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
 conda convert --platform linux-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1

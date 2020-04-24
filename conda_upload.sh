2a7f2f92875ef5ef50eafea51df664cea88c9f97
acwikla <acwikla@novelaneuro.com>
2020-04-24 12:02:04 +0200

    upload conda

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PKG_NAME=ndx-fl-novela
+export VERSION=$(python setup.py)
 #export ANACONDA_API_TOKEN=$CONDA_UPLOAD_TOKEN
 export ANACONDA_API_TOKEN=ac-55c545a3-e7c4-457d-af6b-b123567342a1
 export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
@@ -11,23 +12,31 @@ echo "Building conda package..."
 conda build . -c conda-forge || exit 1
 
 echo "Move conda package..."
-mv ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-*-py37_0.tar.bz2  ${CONDA_BUILD_PATH} || exit 1
+mv ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-${VERSION}-py37_0.tar.bz2  ${CONDA_BUILD_PATH} || exit 1
 
 echo "Making new_tar dir..."
 mkdir ${CONDA_BUILD_PATH}/new_tar || exit 1
 
 echo "Extracting conda package..."
-tar -xf ${CONDA_BUILD_PATH}/${PKG_NAME}-*-py37_0.tar.bz2 -C ${CONDA_BUILD_PATH}/new_tar || exit 1
+tar -xf ${CONDA_BUILD_PATH}/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 -C ${CONDA_BUILD_PATH}/new_tar || exit 1
 
 cd ${CONDA_BUILD_PATH}/new_tar || exit 1
 
 echo "Creating new conda package without some files..."
-tar -cjvf ${PKG_NAME}-*-py37_0.tar.bz2 --exclude='*.sh' --exclude='*.cfg' --exclude='*.pytest_cache' --exclude='*.gitignore' info lib || exit 1
+tar -cjvf ${PKG_NAME}-${VERSION}-py37_0.tar.bz2 --exclude='*.sh' --exclude='*.cfg' --exclude='*.pyc'--exclude='*.pytest_cache' --exclude='*.gitignore' info lib || exit 1
+
+echo 'LSSS'
+ls
 
 cd ..
 
+echo 'LSSSSS'
+pwd
+ls
+
 echo "Move conda package to linux dir..."
-mv new_tar/${PKG_NAME}-*-py37_0.tar.bz2 linux-64 || exit 1
+mv new_tar/${PKG_NAME}-${VERSION}-py37_0.tar.bz2 linux-64 || exit 1
+
 echo "Converting conda package..."
 conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
 conda convert --platform linux-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
@@ -35,5 +44,9 @@ conda convert --platform linux-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --outpu
 conda convert --platform win-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
 conda convert --platform win-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
 
+echo 'LSSS convert osx'
+ls ${CONDA_BUILD_PATH}/osx-64/
+
 echo "Deploying to Anaconda.org..."
 anaconda upload $CONDA_BUILD_PATH/**/$PKG_NAME-*.tar.bz2 --force || exit 1
+

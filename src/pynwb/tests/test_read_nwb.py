62ff3ff74d906c48421b6d46b592e4e8ce7ef9f4
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-04-28 16:30:59 +0200

    read nwb

--- src/pynwb/tests/test_read_nwb.py
+++ src/pynwb/tests/test_read_nwb.py
@@ -1,3 +1,6 @@
+import os
+import unittest
+
 import pynwb
 from pynwb import NWBHDF5IO
 
@@ -7,26 +10,33 @@ from pynwb import NWBFile
 
 from src.pynwb.ndx_fl_novela.probe import Probe, Shank, ShanksElectrode
 
-start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
-create_date = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
 
-nwbfile = NWBFile(
-    session_description='demonstrate NWBFile basics',
-    identifier='NWB123',
-    session_start_time=start_time,
-    file_create_date=create_date
-)
+class TestNWBFileReading(unittest.TestCase):
 
-io = NWBHDF5IO('test.nwb', mode='w')
-shanks = Shank(name='shank_1')
-shanks.add_shanks_electrode(ShanksElectrode('n', 1, 2, 3))
-probe = Probe(name='probe', units='asd', id=1, probe_type='ssd', probe_description='2', num_shanks=3, contact_size=1.0,
-              contact_side_numbering=False)
-probe.add_shanks(shanks)
-nwbfile.add_device(probe)
-io.write(nwbfile)
-io.close()
+    def setUp(self):
+        start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
+        create_date = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
+        nwbfile = NWBFile(
+            session_description='demonstrate NWBFile basics',
+            identifier='NWB123',
+            session_start_time=start_time,
+            file_create_date=create_date
+        )
 
-nwb_file_name = 'test.nwb'
-io = pynwb.NWBHDF5IO('test.nwb', 'r')
-nwbf = io.read()
+        nwb_file_handler = NWBHDF5IO('test.nwb', mode='w')
+        shanks = Shank(name='shank_1')
+        shanks.add_shanks_electrode(ShanksElectrode('n', 1, 2, 3))
+        probe = Probe(name='probe', units='asd', id=1, probe_type='ssd', probe_description='2', num_shanks=3, contact_size=1.0,
+                      contact_side_numbering=False)
+        probe.add_shanks(shanks)
+        nwbfile.add_device(probe)
+        nwb_file_handler.write(nwbfile)
+        nwb_file_handler.close()
+
+    def read_nwb_without_errors(self):
+        self.assertTrue(os.path.exists('test.nwb'))
+        nwb_file_handler = pynwb.NWBHDF5IO('test.nwb', 'r')
+        nwb_file = nwb_file_handler.read()
+
+    def tearDown(self):
+        os.remove('test.nwb')

3a06667bf6a8229e969c578c88265b558b88a46a
acwikla <acwikla@novelaneuro.com>
2020-04-28 15:31:10 +0200

    merge with master

--- src/pynwb/tests/test_read_nwb.py
+++ src/pynwb/tests/test_read_nwb.py
@@ -10,14 +10,18 @@ from src.pynwb.ndx_fl_novela.probe import Probe, Shank, ShanksElectrode
 start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
 create_date = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
 
-nwbfile = NWBFile(session_description='demonstrate NWBFile basics',  # required
-                  identifier='NWB123',  # required
-                  session_start_time=start_time,  # required
-                  file_create_date=create_date)  # optional
+nwbfile = NWBFile(
+    session_description='demonstrate NWBFile basics',
+    identifier='NWB123',
+    session_start_time=start_time,
+    file_create_date=create_date
+)
+
 io = NWBHDF5IO('test.nwb', mode='w')
 shanks = Shank(name='shank_1')
 shanks.add_shanks_electrode(ShanksElectrode('n', 1, 2, 3))
-probe = Probe(name='probe', units='asd', id=1, probe_type='ssd', probe_description='2', num_shanks=3, contact_size=1.0, contact_side_numbering=False)
+probe = Probe(name='probe', units='asd', id=1, probe_type='ssd', probe_description='2', num_shanks=3, contact_size=1.0,
+              contact_side_numbering=False)
 probe.add_shanks(shanks)
 nwbfile.add_device(probe)
 io.write(nwbfile)

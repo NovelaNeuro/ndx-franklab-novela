76aba8fd36113bf0a7faae008251b1cce648df5d
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-06-24 11:34:07 +0200

    direct imports

--- src/pynwb/ndx_franklab_novela/__init__.py
+++ src/pynwb/ndx_franklab_novela/__init__.py
@@ -3,6 +3,14 @@ import os
 from pynwb import load_namespaces
 
 # Set path of the namespace.yaml file to the expected install location
+from src.pynwb.ndx_franklab_novela.associated_files import AssociatedFiles
+from src.pynwb.ndx_franklab_novela.camera_device import CameraDevice
+from src.pynwb.ndx_franklab_novela.data_acq_device import DataAcqDevice
+from src.pynwb.ndx_franklab_novela.header_device import HeaderDevice
+from src.pynwb.ndx_franklab_novela.nwb_electrode_group import NwbElectrodeGroup
+from src.pynwb.ndx_franklab_novela.nwb_image_series import NwbImageSeries
+from src.pynwb.ndx_franklab_novela.probe import Probe
+
 ndx_franklab_novela_specpath = os.path.join(
     os.path.dirname(__file__),
     'spec',
@@ -21,3 +29,5 @@ if not os.path.exists(ndx_franklab_novela_specpath):
 
 # Load the namespace
 load_namespaces(ndx_franklab_novela_specpath)
+
+
